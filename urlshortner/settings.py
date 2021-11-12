77f2e53bad98dfe263ce7daa57638f9c8badd895
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2021-11-12 13:01:20 +0530

    last

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g-59a)xonm@kph_-3c1uudt**tt8aq-q3qsamgkyvm01pek98+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['oururlshortner.herokuapp.com']
 
